01c368e17ef4ab486f35e622ec7a85d8b9ff427c
simple-sifu <kattyntommy@gmail.com>
2023-04-17 23:23:01 -0400

    refactor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import generateJoke from './generateJoke';
 import './styles/main.scss'
+import laughing from './assets/laughing.svg'
+
+const laughImg = document.getElementById('laughImg');
+laughImg.src = laughing;
 
 //console.log("Hello World")
 console.log(generateJoke());
--- webpack.config.js
+++ webpack.config.js
@@ -8,15 +8,23 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
+        // contenthash is to allow for caching
         filename: '[name][contenthash].js',
+        // clean the dist directory of old contenthashes
         clean: true,
+        assetModuleFilename: '[name][ext]',
     },
+    // source-map is useful for debugging and seeing the source code in sources
+    // this will generate a js.map source-map file in dist folder
+    devtool: 'source-map',
     devServer: {
         static: {
             directory: path.resolve(__dirname, 'dist')
         },
         port: 3000,
+        // opens the webpage immediately on startup
         open: true,
+        // hot reload
         hot: true,
         compress: true,
         historyApiFallback: true,
@@ -30,6 +38,20 @@ module.exports = {
                     'css-loader',
                     'sass-loader'
                 ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    }
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource' 
             }
         ]
     },
